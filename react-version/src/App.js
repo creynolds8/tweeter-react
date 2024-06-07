3cf08f8782e7d92e59426076f36028aae2215131
creynolds8 <cameron8.cr@gmail.com>
2024-06-07 15:59:15 -0700

    import component files

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+import TweetForm from './components/TweetForm';
+import TweetList from './components/TweetList';
 
 function App() {
   return (
