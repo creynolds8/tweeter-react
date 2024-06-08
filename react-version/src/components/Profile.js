bd39164ef76b53c18ee31cfcf420403ec0907670
creynolds8 <cameron8.cr@gmail.com>
2024-06-07 17:04:46 -0700

    change jsx to variables over hard coded values

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,13 +1,17 @@
 function Profile() {
+  const avatar = '/profile-hex.png';
+  const firstName = 'Amy';
+  const lastName = 'Mansell';
+
   return (
     <aside>
       <div className="profile">
-        <img className="profile__image" src="profile-hex.png" alt=""/>
+        <img className="profile__image" src={avatar} alt=""/>
       </div>
       <br />
       <div className="profile__name">
         <h2>
-          <span className="profile--bold">Amy</span> Mansell
+          <span className="profile--bold">{firstName}</span> {lastName}
         </h2>
       </div>
     </aside>
