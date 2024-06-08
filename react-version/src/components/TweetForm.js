669cecaca8b5431052acd0eed78f15fc7f876c83
creynolds8 <cameron8.cr@gmail.com>
2024-06-07 17:06:01 -0700

    change jsx to use variable for placeholder

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,11 +1,13 @@
 function TweetForm () {
+  const formText = "What are you humming about?";
+
   return (
     <section className="newtweet">
       <form method="post" action="/tweets" className="newtweet__form">
         <textarea
           className="form__textarea"
           name="text"
-          placeholder="What are you humming about?"
+          placeholder={formText}
         ></textarea>
         <input type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
